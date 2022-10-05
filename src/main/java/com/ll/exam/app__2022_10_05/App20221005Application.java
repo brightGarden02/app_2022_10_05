ed588d4501d70510efb19e74cce775f41cf994b0
brightGarden02 <rreone2017@gmail.com>
2022-10-05 16:16:23 +0900

    feat: 세팅

---
+++ src/main/java/com/ll/exam/app__2022_10_05/App20221005Application.java
@@ -0,0 +1,13 @@
+package com.ll.exam.app__2022_10_05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App20221005Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App20221005Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/exam/app__2022_10_05/App20221005ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.app__2022_10_05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App20221005ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
