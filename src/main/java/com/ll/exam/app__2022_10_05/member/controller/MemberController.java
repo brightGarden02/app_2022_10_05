4aab825b5b957918e68702c37cb6368e13e40c24
brightGarden02 <rreone2017@gmail.com>
2022-10-07 13:48:38 +0900

    feat: response 추가

--- src/main/java/com/ll/exam/app__2022_10_05/member/controller/MemberController.java
+++ src/main/java/com/ll/exam/app__2022_10_05/member/controller/MemberController.java
@@ -6,12 +6,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 @RequestMapping("/member")
 public class MemberController {
 
     @PostMapping("/login")
-    public String login(@RequestBody LoginDto loginDto) {
+    public String login(@RequestBody LoginDto loginDto, HttpServletResponse response) {
+
+        response.addHeader("Authentication", "JWT토큰");
+
         return "username : %s, password : %s".formatted(loginDto.getUsername(), loginDto.getPassword());
     }
 
