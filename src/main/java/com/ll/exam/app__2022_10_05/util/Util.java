eca889d48201f372ef23b2c743fbdf960df885e0
brightGarden02 <rreone2017@gmail.com>
2022-10-07 17:28:28 +0900

    feat: Util.mapOf 구현

--- src/main/java/com/ll/exam/app__2022_10_05/util/Util.java
+++ src/main/java/com/ll/exam/app__2022_10_05/util/Util.java
@@ -5,7 +5,31 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Util {
+
+    public static <K, V> Map<K, V> mapOf(Object... args) {
+
+        Map<K, V> map = new LinkedHashMap<>();
+
+        int size = args.length / 2;
+
+        for (int i = 0; i < size; i++) {
+            int keyIndex = i * 2;
+            int valueIndex = keyIndex + 1;
+
+            K key = (K) args[keyIndex];
+            V value = (V) args[valueIndex];
+
+            map.put(key, value);
+        }
+
+        return map;
+    }
+
+
 
     public static class spring {
 
---
+++ src/test/java/com/ll/exam/app__2022_10_05/util/UtilTest.java
@@ -0,0 +1,27 @@
+package com.ll.exam.app__2022_10_05.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilTest {
+
+
+    @Test
+    @DisplayName("Util.mapOf")
+    void t1() {
+        Map<String, Integer> ages = Util.mapOf("영수", 22, "철수", 33, "영희", 44, "민수", 55);
+
+        assertThat(ages.get("영수")).isEqualTo(22);
+        assertThat(ages.get("철수")).isEqualTo(33);
+        assertThat(ages.get("영희")).isEqualTo(44);
+        assertThat(ages.get("민수")).isEqualTo(55);
+
+    }
+
+
+}
