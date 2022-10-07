11f92d3872badd9ce855f20e073c0a0fe5df40d3
brightGarden02 <rreone2017@gmail.com>
2022-10-07 13:45:49 +0900

    feat: 테스트 케이스 추가

--- src/test/java/com/ll/exam/app__2022_10_05/AuthTests.java
+++ src/test/java/com/ll/exam/app__2022_10_05/AuthTests.java
@@ -6,13 +6,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.nio.charset.StandardCharsets;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -52,5 +55,39 @@ public class AuthTests {
                 .andExpect(status().is2xxSuccessful());
 
     }
+
+
+    @Test
+    @DisplayName("POST /member/login으로 올바른 username과 password 데이터를 넘기면 JWT키를 발급해준다.")
+    void v2() throws Exception {
+
+        //when
+        ResultActions resultActions = mvc
+                .perform(
+                        post("/member/login")
+                                .content("""
+                                        {
+                                            "username": "user1",
+                                            "password": "1234"
+                                        }
+                                        """.stripIndent())
+                                .contentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
+                )
+                .andDo(print());
+
+        //then
+        resultActions.andExpect(status().is2xxSuccessful());
+
+        MvcResult mvcResult = resultActions.andReturn();
+
+        MockHttpServletResponse response = mvcResult.getResponse();
+
+        String authentication = response.getHeader("Authentication");
+
+        assertThat(authentication).isNotEmpty();
+
+
+    }
+
 
 }
