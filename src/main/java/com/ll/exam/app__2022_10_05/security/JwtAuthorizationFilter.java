a36da311eaf1c94e9f0d847043347093b772450f
brightGarden02 <rreone2017@gmail.com>
2022-10-07 19:12:00 +0900

    feat: 로그인, 회원가입 엔드포인트 제외하고 전무 인증필요, 필터추카

---
+++ src/main/java/com/ll/exam/app__2022_10_05/security/JwtAuthorizationFilter.java
@@ -0,0 +1,22 @@
+package com.ll.exam.app__2022_10_05.security;
+
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.debug("JwtAuthorizationFilter 실행됨");
+    }
+}
--- src/main/java/com/ll/exam/app__2022_10_05/security/SecurityConfig.java
+++ src/main/java/com/ll/exam/app__2022_10_05/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.*;
 
@@ -14,16 +15,27 @@ import static org.springframework.security.config.http.SessionCreationPolicy.*;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationConfiguration authenticationConfiguration) throws Exception {
         http
+                .authorizeRequests(
+                        authorizeRequests -> authorizeRequests
+                                .antMatchers("/member/login", "/member/join")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated() //최소자격 : 로그인
+                )
                 .cors().disable() //타 도메인에서 API 호출 가능
                 .csrf().disable() //CSRF 토큰 끄기
                 .httpBasic().disable() //httpBasic 로그인 방식 끄기
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(STATELESS)
-                ); //세션 사용안함
+                ).addFilterBefore(
+                        jwtAuthorizationFilter,
+                UsernamePasswordAuthenticationFilter.class
+                );
 
         return http.build();
     }
