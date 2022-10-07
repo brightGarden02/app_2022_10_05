c91ff4dc2cb18bb3ec5f0c707dac9d457ff14bd3
brightGarden02 <rreone2017@gmail.com>
2022-10-07 19:23:18 +0900

    feat: 필터에서 요청헤더의 Authorization 필드에 토큰값이 유효한지 체크후 MemberContext 객체 생성후 스프링 시큐리티에 등록

--- src/main/java/com/ll/exam/app__2022_10_05/member/controller/MemberContext.java
+++ src/main/java/com/ll/exam/app__2022_10_05/member/controller/MemberContext.java
@@ -1,21 +1,39 @@
 package com.ll.exam.app__2022_10_05.member.controller;
 
+import com.fasterxml.jackson.annotation.JsonIncludeProperties;
+import com.ll.exam.app__2022_10_05.member.entity.Member;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+
+@Getter
+@JsonIncludeProperties({"id", "createDate", "modifyDate", "username", "email", "authorities"})
 public class MemberContext extends User {
 
-    public MemberContext(String username, String password, Collection<? extends GrantedAuthority> authorities) {
-        super(username, password, authorities);
-    }
+    private final long id;
+    private final LocalDateTime createDate;
+    private final LocalDateTime modifyDate;
+    private final String username;
+    private final String email;
+    private final Set<GrantedAuthority> authorities;
 
-    public MemberContext(String username, String password, boolean enabled, boolean accountNonExpired,
-                         boolean credentialNonExpired, boolean accountNonlocked,
-                         Collection<? extends GrantedAuthority> authorities) {
 
-        super(username, password, enabled, accountNonExpired, credentialNonExpired, accountNonlocked, authorities);
+    public MemberContext(Member member) {
+
+        super(member.getUsername(), "", member.getAuthorities());
+
+        id = member.getId();
+        createDate = member.getCreateDate();
+        modifyDate = member.getModifyDate();
+        username = member.getUsername();
+        email = member.getEmail();
+        authorities = member.getAuthorities().stream().collect(Collectors.toSet());
     }
 
 }
--- src/main/java/com/ll/exam/app__2022_10_05/security/JwtAuthorizationFilter.java
+++ src/main/java/com/ll/exam/app__2022_10_05/security/JwtAuthorizationFilter.java
@@ -1,7 +1,16 @@
 package com.ll.exam.app__2022_10_05.security;
 
+import com.ll.exam.app__2022_10_05.member.controller.MemberContext;
+import com.ll.exam.app__2022_10_05.member.entity.Member;
+import com.ll.exam.app__2022_10_05.member.service.MemberService;
+import com.ll.exam.app__2022_10_05.seciroty.kwt.JwtProvider;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -10,13 +19,50 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+    private final MemberService memberService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        log.debug("JwtAuthorizationFilter 실행됨");
+//        log.debug("JwtAuthorizationFilter 실행됨");
+        String bearerToken = request.getHeader("Authorization");
+
+        if(bearerToken != null) {
+            String token = bearerToken.substring("Bearer ".length());
+
+            if(jwtProvider.verify(token)) {
+                Map<String, Object> claims = jwtProvider.getClaims(token);
+                String username = (String) claims.get("username");
+                Member member = memberService.findByUsername(username).orElseThrow(
+                        () -> new UsernameNotFoundException("'%s' Username not found.".formatted(username))
+                );
+
+                forceAuthentication(member);
+
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private void forceAuthentication(Member member) {
+        MemberContext memberContext = new MemberContext(member);
+
+        UsernamePasswordAuthenticationToken authentication = UsernamePasswordAuthenticationToken.authenticated(
+                memberContext,
+                null,
+                member.getAuthorities()
+        );
+
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        context.setAuthentication(authentication);
+        SecurityContextHolder.setContext(context);
     }
 }
